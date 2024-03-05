c3419da2fe7c62cc1bb663348bfcd4456b25de97
mustafa.mr <mustafa.mr@caliberuniversal.com>
2024-03-05 12:18:24 +0530

    home screen changes added

--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
-import { PageModule } from '@abp/ng.components/page';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {
+  NgbCollapseModule,
+  NgbDatepickerModule,
+  NgbDropdownModule,
+  NgbOffcanvasModule,
+  NgbAccordionModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { CoreModule } from '@abp/ng.core';
+import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { NgxValidateCoreModule } from '@ngx-validate/core';
+import { CommercialUiModule } from '@volo/abp.commercial.ng.ui';
+import { PageModule } from '@abp/ng.components/page';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, HomeRoutingModule, PageModule],
+  imports: [SharedModule, HomeRoutingModule, PageModule,
+    NgbModule,
+    CommonModule,
+    CoreModule,
+    ThemeSharedModule,
+    CommercialUiModule,
+    NgxValidateCoreModule,
+    NgbCollapseModule,
+    NgbDatepickerModule,
+    NgbDropdownModule,
+    NgbOffcanvasModule,
+    NgbAccordionModule,
+    // BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxChartsModule,
+    NgApexchartsModule
+  ],
 })
 export class HomeModule {}
