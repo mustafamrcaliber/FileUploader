324ae06cad3b284a4d126bb86467e2963b79ee9c
mustafa.mr <mustafa.mr@caliberuniversal.com>
2024-03-28 12:35:54 +0530

    added chnages

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,7 +3,7 @@ import { Environment } from '@abp/ng.core';
 const baseUrl = 'http://localhost:4200';
 
 const oAuthConfig = {
-  issuer: 'http://192.168.0.101:44377/',
+  issuer: 'http://172.20.10.2:44377/',
   redirectUri: baseUrl,
   clientId: 'FileUploader_App',
   responseType: 'code',
@@ -20,7 +20,7 @@ export const environment = {
   oAuthConfig,
   apis: {
     default: {
-      url: 'http://192.168.0.101:44335',
+      url: 'http://172.20.10.2:44335',
       rootNamespace: 'FileUploader',
     },
     AbpAccountPublic: {
